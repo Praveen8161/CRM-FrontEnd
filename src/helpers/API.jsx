4397ccdca63efe2cfe1fb09e8deadb15b3ee2db3
Praveen8161 <rbalu8161@gmail.com>
2023-12-29 21:44:28 +0530

    url update

--- src/helpers/API.jsx
+++ src/helpers/API.jsx
@@ -1,2 +1,2 @@
 export const API = "https://crm-backend-dhvw.onrender.com";
-export const CurrAPI = "https://crm-frontend-js.netlify.app/";
+export const CurrAPI = "https://crm-frontend-js.netlify.app";
